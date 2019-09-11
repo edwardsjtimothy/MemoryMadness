43a9f270bded559d640d5c73b5ef75f20b1566cf
Enragedtoast <edwardsjtimothy@gmail.com>
2019-09-11 12:34:07 -0500

    images displaying correctly

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
 import Gameboard from "./components/Gameboard";
-// import Wrapper from "./components/Wrapper";
-import Gamepiece from "./components/Gamepiece";
-import image from "./images.json";
 import "./App.css"
 
 
