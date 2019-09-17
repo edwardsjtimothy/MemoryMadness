2616105079d5eaf86787b1980de034f8d2422444
Enragedtoast <edwardsjtimothy@gmail.com>
2019-09-17 18:26:31 -0500

    Updates

---
+++ precache-manifest.9f0aee48dce1f2c5baebc926d998c279.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4222aa9466eadefc7d4dabe69134a36",
+    "url": "/MemoryMadness/index.html"
+  },
+  {
+    "revision": "6e3906e123d6215c59ac",
+    "url": "/MemoryMadness/static/css/main.8aef1152.chunk.css"
+  },
+  {
+    "revision": "0cb23a97bb4e6308e4ed",
+    "url": "/MemoryMadness/static/js/2.18a842fd.chunk.js"
+  },
+  {
+    "revision": "6e3906e123d6215c59ac",
+    "url": "/MemoryMadness/static/js/main.e13bd8e9.chunk.js"
+  },
+  {
+    "revision": "a65417ca64a71e990697",
+    "url": "/MemoryMadness/static/js/runtime~main.70b95927.js"
+  },
+  {
+    "revision": "4433aaa750039f0259319d7da948a0c7",
+    "url": "/MemoryMadness/static/media/jumbo-back.4433aaa7.jpg"
+  },
+  {
+    "revision": "b9da2244b48e97577590dd8137200f51",
+    "url": "/MemoryMadness/static/media/memphis-mini-dark.b9da2244.png"
+  }
+]);
